09b9977e06fcd899704fea522cf29d239fa717e4
MattiaLaMura <mattia.la-mura@alumni.univ-avignon.fr>
2022-05-04 15:55:59 +0200

    TP3 > Ajout tests pour IPokemonMetadataProvider

--- projet_maven/src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
+++ projet_maven/src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -1,15 +1,34 @@
 package fr.univavignon.pokedex.api;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
 
-public class IPokemonMetadataProviderTest extends TestCase {
+import org.junit.Test;
+import org.mockito.Mockito;
 
-	public IPokemonMetadataProviderTest(String name) {
-		super(name);
-	}
-
-	public void testIfTrue() {
-		assertTrue(true);
+public class IPokemonMetadataProviderTest {	
+	
+	@Test
+	public void testMetadata() throws PokedexException {
+		
+		IPokemonMetadataProvider iPokemonMetadataProviderMock = Mockito.mock(IPokemonMetadataProvider.class);
+		PokemonMetadata pokemonMetadata = new PokemonMetadata(0,"Bulbizarre",126,126,90);
+		
+		when(iPokemonMetadataProviderMock.getPokemonMetadata(anyInt())).thenAnswer(input -> {
+			int index = input.getArgument(0);
+			if(index > 150 || index < 0) {
+				throw new PokedexException("Index out of range");
+			}
+			else {
+				return pokemonMetadata;
+			}
+		});
+		
+		assertEquals(pokemonMetadata, iPokemonMetadataProviderMock.getPokemonMetadata(0));
+		assertThrows(PokedexException.class,()->iPokemonMetadataProviderMock.getPokemonMetadata(-1));
+		assertThrows(PokedexException.class,()->iPokemonMetadataProviderMock.getPokemonMetadata(155));
+		
 	}
 
 }
